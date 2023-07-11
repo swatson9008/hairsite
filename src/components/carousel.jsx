9dba7b59ac8bd714f1ad39452021efe0d9d7e493
Summer <swatson9008@yahoo.com>
2023-07-11 18:17:05 -0400

    fixed the carousel

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -12,7 +12,7 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 
-import './styles.css';
+
 
 // import required modules
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
