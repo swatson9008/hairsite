18d844ad1a8358772daf756942445dab8b1aca79
Summer <swatson9008@yahoo.com>
2023-07-11 16:55:14 -0400

    files up

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-
+import Nav from "./components/navBar"
 
 function App() {
   
 
   return (
     <main>
-      
+      <Nav />
     </main>
   )
 }
---
+++ src/components/navBar.jsx
@@ -0,0 +1,5 @@
+export default function Nav(){
+    return (
+        <div><span>Hair by Savannah</span></div>
+    )
+}
