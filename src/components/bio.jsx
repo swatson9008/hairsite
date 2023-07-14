c6458235c7efa0a266690eb12f3abb7f5055273d
Summer <swatson9008@yahoo.com>
2023-07-14 11:57:35 -0400

    mobile view now works okay

--- src/components/bio.jsx
+++ src/components/bio.jsx
@@ -1,9 +1,11 @@
 import scissors from '../assets/scissors.svg'
 import comb from '../assets/comb.svg'
 import { MotionAnimate } from 'react-motion-animate'
+import profilePic from '../assets/profileimg.jpg'
 
 export default function Bio() {
     return (
+        <div className='bioAdjust'>
         <div className="bioMain">
             <div className="scissors">
                 <MotionAnimate 
@@ -15,10 +17,12 @@ export default function Bio() {
                         <img src={scissors} alt=""/>
                 </MotionAnimate></div>
             <div className="circle">
-                <div className="circleText">some sample text</div>
+                <div className="circleText"><div className='insiderPic'><img src={profilePic} alt=""/></div>
+                    <span>Savannah has been working as a hairstylist since 2014. Her salon accepts customers by appointment only</span></div>
             </div>
             <div className="comb">
-                <MotionAnimate 
+                <MotionAnimate
+                    className="combContain"
                     animation='fadeInUp' 
                     delay={0.2}
                     speed={1.5}
@@ -26,6 +30,7 @@ export default function Bio() {
                     reset={true}>
                         <img src={comb} alt=""/>
                 </MotionAnimate></div>
+        </div>
         </div>
     )
 }
